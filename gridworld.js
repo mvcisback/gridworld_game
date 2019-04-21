de24a81efdcba7cfb4dab553798f8ca567e31fd6
Marcell Vazquez-Chanlatte <mvc@linux.com>
2019-04-20 17:54:12 -0700

    start coding up gridworld

---
+++ gridworld.js
@@ -0,0 +1,102 @@
+const x0 = 20;
+const y0 = 20;
+const dx = 38;
+const xmax = 20;
+const actions = new Set([
+    'ArrowRight',
+    'ArrowLeft',
+    'ArrowUp',
+    'ArrowDown'
+]);
+
+const step_counter = document.getElementById('step-count');
+
+
+var time = 0;
+
+var state = {
+    x: 0,
+    y: 0,
+    bx: 0,
+    by: 0,
+};
+
+
+var agent;
+var gridworld_s = new Snap('#gridworld');
+Snap.load('imgs/gridworld.svg', function (response) {
+    gridworld_s.append(response);
+    agent = gridworld_s.circle(x0, x0, "0.3cm");
+    agent.attr({class: "agent"});
+});
+var keys_s = new Snap('#keys');
+Snap.load('imgs/Qwerty.svg', function (response) {
+    keys_s.append(response);
+});
+
+
+function move(keyName){
+    if (!actions.has(keyName)) {
+        return;
+    }
+    time += 1;
+
+    switch (keyName) {
+    case 'ArrowRight':
+        state.x += 1;
+        break;
+    case'ArrowLeft':
+        state.x -= 1;
+        break;
+    case'ArrowUp':
+        state.y -= 1;
+        break;
+    case'ArrowDown':
+        state.y += 1;
+        break;
+    }
+
+    // TODO: Animate bump.
+    if (state.x > 7) {
+        state.bx = +12;
+        state.x = 7;
+    }
+    else if (state.y > 7) {
+        state.by = +12;
+        state.y = 7;
+    }
+    else if (state.y < 0) {
+        state.by = -12;
+        state.y = 0;
+    }
+    else if (state.x < 0) {
+        state.bx = -12;
+        state.x = 0;
+    }
+}
+
+function redraw(){
+    agent.animate(
+        {cx: x0 + dx*state.x + state.bx, cy: x0 + dx*state.y + state.by},
+        100, 
+        mina.ease,
+        () => {
+            agent.animate(
+                {cx: x0 + dx*state.x, cy: x0 + dx*state.y},
+                100, 
+                mina.bounce
+            );
+        }
+    );
+    state.bx = 0;
+    state.by = 0;
+    step_counter.textContent = time;
+    step_counter.setAttribute("value", 100 - time);
+}
+
+
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    move(keyName);
+    redraw();
+}, false);
